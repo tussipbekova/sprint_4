863d21d896217b9491800c53691812717bf9b28d
Saya Tussipbekova <tussipbekova@gmail.com>
2023-11-14 22:42:27 +0600

    Fixed test_add_new_book_add_two_books and added gitignore

--- tests.py
+++ tests.py
@@ -14,11 +14,11 @@ class TestBooksCollector:
 
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book('Гарри Поттер')
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre.keys()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
